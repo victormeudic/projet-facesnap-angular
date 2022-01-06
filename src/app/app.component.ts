a7cd01ebe1dc693c5c04ae15edd54897e9ea2b13
Victor Meudic <victormeudic@gamil.com>
2022-01-06 15:08:03 +0100

    nettoyage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -7,36 +6,5 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
-
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Mes vacances',
-        description: 'Photos de mes vacances',
-        imageUrl:
-          'https://media.istockphoto.com/photos/small-retro-car-with-baggage-luggage-and-beach-equipment-on-the-roof-picture-id1140063009?k=20&m=1140063009&s=612x612&w=0&h=v4h02KSCtiYNn90v8nt-6jV47cNf4aDS0pNKzx-88R8=',
-        createdDate: new Date(),
-        snaps: 149,
-        location: 'Noirmoutier',
-      },
-      {
-        title: 'Ma nouvelle maison',
-        description: 'Je vous présente ma nouvelle maison',
-        imageUrl:
-          'https://www.maisonsclairlogis.fr/wp-content/uploads/maison-contemporaine_onyx-version-nuit.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Los Angeles',
-      },
-      {
-        title: 'Mon bateau',
-        description: 'Petite balade en bateau',
-        imageUrl:
-          'https://img.nauticexpo.fr/images_ne/projects/images-g/nouveau-plate-forme-bateau-exterieur-mastercraft-12403-9874230.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-      },
-    ];
-  }
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, FaceSnapComponent],
+  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent, HeaderComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
